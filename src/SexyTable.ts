e5a63fd02ec4dbf4be81de3099aa966ac8940302
Brad Jones <brad@bjc.id.au>
2015-06-01 19:31:37 +1000

    Major changes and refactorings.

--- src/SexyTable.ts
+++ src/SexyTable.ts
@@ -15,137 +15,57 @@ module SexyTable
     /**
      * If set to true we will search for all sexy tables at dom ready and run
      * the automatic width calculations. And then we will show the table.
-     *
-     * @type boolean
      */
     export var AutoMakeSexy: boolean = true;
 
     /**
      * Finds All Sexy Tables in the DOM and Initialises Them.
-     * If ```AutoMakeSexy``` is true we will run at domready.
      */
-    function MakeTablesSexy()
-    {
-        $('.sexy-table').each(function(index, table)
-        {
-            new TableSizeCalculator(table);
-        });
-    }
-
-    /**
-     * This is what emulates the auto sizing features of a normal HTML <table>.
-     * It's not 100% perfect yet and I hate to think how it will perform in
-     * older versions of IE. But it does basically work.
-     */
-    export class TableSizeCalculator
-    {
-        private container: JQuery;
-
-        private correctionFactor: number = 1.1;
-
-        public constructor(table: Element, correctionFactor?: number)
-        {
-            this.container = $(table);
-
-            if (correctionFactor != null)
-            {
-                this.correctionFactor = correctionFactor;
-            }
-
-            this.InsertMissingCells();
-
-            this.InsertCellWrapper();
-
-            this.SetWidthOfCells();
-
-            this.SetHeightOfRows();
-
-            this.UnhideContainer();
-
-            $(window).resize(this.SetWidthOfCells.bind(this));
-            $(window).resize(this.SetHeightOfRows.bind(this));
-        }
-
-        private InsertMissingCells(): void
-        {
-            // TODO... the idea here is to automatically create any missing
-            // li elements. Kind of like automatic colspans.
-        }
-
-        private InsertCellWrapper(): void
-        {
-            this.container.find('li').wrapInner('<div class="inner"></div>');
-        }
-
-        private SetHeightOfRows(): void
-        {
-            var that = this;
-
-            this.container.find('ul').css('height', 'auto');
-
-            this.container.find('ul').each(function(index, row)
-            {
-                $(row).css('height', that.CalculateRowHeight(row));
-            });
-        }
-
-        private CalculateRowHeight(row: Element): number
-        {
-            var maxHeight = -1;
-
-            $(row).find('li').each(function(index, cell)
-            {
-                if ($(cell).outerHeight(true) > maxHeight)
-                {
-                    maxHeight = $(cell).outerHeight(true);
-                }
-            });
-
-            return maxHeight;
-        }
-
-        private SetWidthOfCells(): void
-        {
-            this.container.find('li').css('width', this.CalculateCellWidth());
-        }
-
-        private CalculateCellWidth(): number
-        {
-            var cols = this.GetNumberOfCols();
-            var width = this.GetTotalWidthOfTable();
-            var padding = this.GetCellPadding();
-            return (width / cols) - padding - this.correctionFactor;
-        }
-
-        private UnhideContainer(): void
-        {
-            this.container.css('visibility', 'visible');
-        }
-
-        private GetTotalWidthOfTable(): number
-        {
-            return this.container.innerWidth();
-        }
-
-        private GetNumberOfCols(): number
-        {
-            return this.container.find('ul').first().find('li').length;
-        }
-
-        private GetCellPadding(): number
-        {
-            var firstCell = this.container.find('li').first();
-
-            return firstCell.outerWidth(true) - firstCell.width();
-        }
-    }
-
-    // 3... 2... 1... and LiftOff!!!
     $(document).ready(function()
     {
         if (AutoMakeSexy)
         {
-            MakeTablesSexy();
+            $('.sexy-table').each(function(index, table)
+            {
+                new Table(table);
+            });
         }
     });
+
+    /**
+     * IE8 Bind Polyfill
+     *
+     * @see https://goo.gl/5J4TJq
+     */
+    if (!Function.prototype.bind)
+    {
+        Function.prototype.bind = function(oThis)
+        {
+            if (typeof this !== 'function')
+            {
+                throw new TypeError
+                (
+                    'Function.prototype.bind - ' +
+                    'what is trying to be bound is not callable'
+                );
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP    = function() {},
+            fBound  = function()
+            {
+                return fToBind.apply
+                (
+                    this instanceof fNOP && oThis ? this : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments))
+                );
+            };
+
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
 }
