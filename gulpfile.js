9a228049abe35ee9d5e686fb3672d453de359c5f
Brad Jones <brad@bjc.id.au>
2015-10-01 16:10:19 +1000

    Updated gulpfile and build/release process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,49 @@
-var del = require('del');
-var path = require('path');
-var gulp = require('gulp');
-var merge = require('merge2');
-var rename = require('gulp-rename');
-var less = require('gulp-less');
-var ts = require('gulp-typescript');
-var uglify = require('gulp-uglify');
-var uglifycss = require('gulp-uglifycss');
-var autoprefixer = require('gulp-autoprefixer');
+// Import some modules
+var
+    Q =            require('q'),
+    del =          require('del'),
+    merge =        require('merge2'),
+    gulp =         require('gulp'),
+    run =          require('gulp-run'),
+    bump =         require('gulp-bump'),
+    rename =       require('gulp-rename'),
+    less =         require('gulp-less'),
+    ts =           require('gulp-typescript'),
+    uglify =       require('gulp-uglify'),
+    uglifycss =    require('gulp-uglifycss'),
+    autoprefixer = require('gulp-autoprefixer'),
+    gitTagV =      require('gulp-tag-version')
+;
 
-gulp.task('default', ['clean-build', 'compile-ts', 'compile-less'], function()
+/**
+ * Runs after ```npm install```, performs some final setup.
+ */
+gulp.task('npm-postinstall', function(done)
 {
-    gulp.watch('./src/**/*.ts', ['compile-ts']);
-    gulp.watch('./src/**/*.less', ['compile-less']);
+    run('npm install transparency@^0.10.0 --ignore-scripts').exec(function()
+    {
+        run('tsd reinstall').exec(function()
+        {
+            run('tsd rebundle').exec(function()
+            {
+                run('git checkout typings/custom.d.ts').exec(done);
+            });
+        });
+    });
 });
 
-gulp.task('clean-build', function (cb)
+/**
+ * Deletes everything inside the dist folder
+ * to ensure we don't get stale artifacts.
+ */
+gulp.task('clean', function (done)
 {
-    del(['dist/**/*'], cb);
+    del(['./dist/**/*'], done);
 });
 
+/**
+ * Compiles all typescript source files into javascript.
+ */
 gulp.task('compile-ts', function()
 {
     // Read in the TypeScript Project
@@ -54,6 +78,9 @@ gulp.task('compile-ts', function()
     // TODO: Sourcemap...
 });
 
+/**
+ * Compiles all less source files into css.
+ */
 gulp.task('compile-less', function()
 {
     return gulp.src('./src/**/*.less')
@@ -76,4 +103,64 @@ gulp.task('compile-less', function()
 
         // TODO: Sourcemap...
     ;
+});
+
+/**
+ * Creates a fresh build from all source files.
+ */
+gulp.task('build', ['clean', 'compile-ts', 'compile-less']);
+
+/**
+ * Watches source for changes and will continuously re-build until stopped.
+ */
+gulp.task('watch', function()
+{
+    gulp.watch('./src/**/*.ts', ['compile-ts']);
+    gulp.watch('./src/**/*.less', ['compile-less']);
+});
+
+/**
+ * Increments the version number of the main package.json file.
+ */
+gulp.task('bump', function()
+{
+    gulp.src('./package.json')
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+/**
+ * Creates a new git tag and pushes to github.
+ */
+gulp.task('tag', ['build', 'bump'], function()
+{
+    var deferred = Q.defer();
+
+    var complete = 0, done = function()
+    {
+        ++complete; if (complete == 2) deferred.resolve();
+    };
+
+    run('git add -A').exec(function()
+    {
+        run("git commit -m NewBuild").exec(function()
+        {
+            run('git push').exec(done);
+
+            gulp.src('./package.json').pipe(gitTagV()).on('end', function()
+            {
+                run('git push --tags').exec(done);
+            });
+        });
+    });
+
+    return deferred.promise;
+});
+
+/**
+ * Publishes the npm package to npmjs.com
+ */
+gulp.task('publish', ['tag'], function(done)
+{
+    run("npm publish").exec(done);
 });
